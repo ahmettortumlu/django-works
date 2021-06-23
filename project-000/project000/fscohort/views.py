aba83625b10b84989e1cdbba81eb5080d5f1996c
ahmettortumlu <tortumlu-atlassian-4r0hf7@deepinfo.com>
2021-06-23 15:02:49 +0300

    added fscohort app

---
+++ project-000/project000/fscohort/views.py
@@ -0,0 +1,14 @@
+# HttpResponse is used to 
+# pass the information  
+# back to view 
+from django.http import HttpResponse
+
+# Defining a function which 
+# will receive request and 
+# perform task depending  
+# upon function definition 
+def index(request):
+
+    # This will return Hello, world. This is fscohort index page.
+    # string as HttpResponse 
+    return HttpResponse("Hello, world. This is fscohort app index page.")
--- project-000/project000/project000/settings.py
+++ project-000/project000/project000/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # myapps
+    'fscohort'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
